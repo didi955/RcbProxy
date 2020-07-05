cac86713aff138713ee75fec4c15490eace64239
BuildTools <unconfigured@null.spigotmc.org>
2020-07-05 11:31:52 +0200

    0.1.3 AutoCompletion fix

--- src/fr/rushcubeland/rcbproxy/bungee/AutoCompletion.java
+++ src/fr/rushcubeland/rcbproxy/bungee/AutoCompletion.java
@@ -8,6 +8,8 @@ import net.md_5.bungee.api.event.TabCompleteEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 public class AutoCompletion implements Listener {
@@ -35,7 +37,6 @@ public class AutoCompletion implements Listener {
                     }
                     if (args.length == 2 && getSpace(e.getCursor()) == 1) {
                         addSuggestions(e, args);
-
                         return;
                     }
                     if (args.length == 2) {
@@ -61,6 +62,10 @@ public class AutoCompletion implements Listener {
                         }
                         return;
                     }
+                    if (args.length == 2 && getSpace(e.getCursor()) == 1) {
+                        addSuggestions(e, args);
+                        return;
+                    }
                 }
                 if(MuteCommand.getCmd().contains(args[0].replaceAll("/", "")) && e
                         .getCursor().contains(" ")) {
@@ -73,6 +78,10 @@ public class AutoCompletion implements Listener {
                         for (ProxiedPlayer all : ProxyServer.getInstance().getPlayers()) {
                             e.getSuggestions().add(all.getName());
                         }
+                        return;
+                    }
+                    if (args.length == 2 && getSpace(e.getCursor()) == 1) {
+                        addSuggestions(e, args);
                         return;
                     }
                 }
@@ -89,6 +98,10 @@ public class AutoCompletion implements Listener {
                         }
                         return;
                     }
+                    if (args.length == 2 && getSpace(e.getCursor()) == 1) {
+                        addSuggestions(e, args);
+                        return;
+                    }
                 }
                 if(UnmuteCommand.getCmd().contains(args[0].replaceAll("/", "")) && e
                         .getCursor().contains(" ")) {
@@ -101,6 +114,10 @@ public class AutoCompletion implements Listener {
                         for (ProxiedPlayer all : ProxyServer.getInstance().getPlayers()) {
                             e.getSuggestions().add(all.getName());
                         }
+                        return;
+                    }
+                    if (args.length == 2 && getSpace(e.getCursor()) == 1) {
+                        addSuggestions(e, args);
                         return;
                     }
                 }
@@ -117,6 +134,10 @@ public class AutoCompletion implements Listener {
                         }
                         return;
                     }
+                    if (args.length == 2 && getSpace(e.getCursor()) == 1) {
+                        addSuggestions(e, args);
+                        return;
+                    }
                 }
                 if(KickCommand.getCmd().contains(args[0].replaceAll("/", "")) && e
                         .getCursor().contains(" ")) {
@@ -129,6 +150,10 @@ public class AutoCompletion implements Listener {
                         for (ProxiedPlayer all : ProxyServer.getInstance().getPlayers()) {
                             e.getSuggestions().add(all.getName());
                         }
+                        return;
+                    }
+                    if (args.length == 2 && getSpace(e.getCursor()) == 1) {
+                        addSuggestions(e, args);
                         return;
                     }
                 }
@@ -145,6 +170,10 @@ public class AutoCompletion implements Listener {
                         }
                         return;
                     }
+                    if (args.length == 2 && getSpace(e.getCursor()) == 1) {
+                        addSuggestions(e, args);
+                        return;
+                    }
                 }
                 if(FriendCommand.getCmds().contains(args[0].replaceAll("/", "")) && e
                 .getCursor().contains(" ")){
@@ -157,6 +186,10 @@ public class AutoCompletion implements Listener {
                         e.getSuggestions().add("add");
                         e.getSuggestions().add("accept");
                         e.getSuggestions().add("deny");
+                    }
+                    if (args.length == 2 && getSpace(e.getCursor()) == 1) {
+                        addSuggestions(e, args);
+                        return;
                     }
                     if(args.length == 2){
                         if(args[1].equalsIgnoreCase("remove") || args[1].equalsIgnoreCase("rm") || args[1].equalsIgnoreCase("del") || args[1].equalsIgnoreCase("delete")){
@@ -173,6 +206,9 @@ public class AutoCompletion implements Listener {
                             }
                         }
                     }
+                    if (args.length == 3 && getSpace(e.getCursor()) == 2) {
+                        addSuggestions(e, args);
+                    }
                 }
             }
         }
@@ -180,13 +216,20 @@ public class AutoCompletion implements Listener {
 
     private void addSuggestions(TabCompleteEvent e, String[] args) {
         String check = args[args.length - 1];
+        List<String> argFriend = Arrays.asList("list", "remove", "deny", "accept", "add");
 
         for (ProxiedPlayer all : ProxyServer.getInstance().getPlayers()) {
             if (all.getName().toLowerCase().startsWith(check)) {
                 e.getSuggestions().add(all.getName());
             }
         }
+        for(String s : argFriend){
+            if(s.startsWith(check)){
+                e.getSuggestions().add(s);
+            }
+        }
     }
+
 
     public static int getSpace(String s) {
         int space = 0;
