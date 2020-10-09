f6a43e62f662ab4f69c35298c5eac8df5fc581d6
Dylan l <45293702+didi955@users.noreply.github.com>
2020-10-09 18:32:10 +0200

    Update DatabaseManager.java

--- src/fr/rushcubeland/rcbproxy/bukkit/data/sql/DatabaseManager.java
+++ src/fr/rushcubeland/rcbproxy/bukkit/data/sql/DatabaseManager.java
@@ -2,7 +2,7 @@ package fr.rushcubeland.rcbproxy.bukkit.data.sql;
 
 public enum DatabaseManager {
 
-    Main_BDD(new DatabaseCredentials("sql.alls-heberg.fr", "DylanL17833", "c2OxJa7Ltjsf0dN", "DylanL17833", 3306));
+    Main_BDD(new DatabaseCredentials("*******", "*****", "******", "*****", 3306));
 
     private DatabaseAccess databaseAccess;
 
